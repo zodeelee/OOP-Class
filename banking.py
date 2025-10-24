9792a26c21bf84abfe42640decd533317dd17825
zl <zodeelee@gmail.com>
2025-10-24 08:26:15 -0500

    updated banking.py

--- banking.py
+++ banking.py
@@ -1,19 +1,27 @@
 class Customer:
     def __init__(self):
-        self.cid = input("Enter the customer ID:")
-        self.acc_no = input("Enter the account number:")
-        self.cname = input("Enter the customer name:")
-        self.phone = input("Enter the phone number:")
-        self.emailid = input("Enter the email:")
-        self.balance = int(input("Enter the account balance:"))
+        self.cid = ""
+        self.acc_no = ""
+        self.cname = ""
+        self.phone = ""
+        self.emailid = ""
+        self.balance = 0
 
     def debit_from(self, amt):
-        self.balance -= input("amt:")
+        self.balance -= float(input("Enter the amount to send:"))
 
     def credit_to(self, amt):
-        self.balance += input("amt:")
+        self.balance += float(input("Enter the amount to receive:"))
 
-    def display(self):
+    def add_customer(self):
+        self.cid = input("Enter customer ID:")
+        self.acc_no = input("Enter account number:")
+        self.cname = input("Enter customer name:")
+        self.phone = input("Enter phone number:")
+        self.emailid = input("Enter email ID:")
+        self.balance = int(input("Enter balance:"))
+
+    def display_info(self):
         print("\n[Customer ID]:", self.cid,
               "\n [Account Number]:", self.acc_no,
               "\n [Customer Name]:", self.cname,
@@ -23,19 +31,16 @@ class Customer:
 
 #<><><><><><><><<>><><><><<><><>MAINCODE<><><><><><><><><><<><><><><><><><><><><>
 
-while 1:
-    print("<-------------------------------------------------->"
-          "\n1. Select Debit Option",
-          "\n2. Select Credit Option",
-          "\n2. Exit\n"
-          "<-------------------------------------------------->\n")
-    option = input("[Enter your choice]:")
 
+c1 = Customer()
+c2 = Customer()
 
+c1.add_customer()
+c2.add_customer()
 
-    c1 = Customer()
-    c2 = Customer()
+c1.debit_from(amt=100)
+c2.credit_to(amt=-100)
 
-    c1.display()
-    c2.display()
+c1.display_info()
+c2.display_info()
 
