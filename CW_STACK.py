525d97cda5b48b62c87c10ae63051f8461a130e9
zl <zodeelee@gmail.com>
2025-11-12 09:51:08 -0600

    Add files via upload

---
+++ CW_STACK.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.element = []
+    def push(self):
+        self.element.append((text_Box.get(1.0, "end-1c"))
+    def pop(self):
+        self.element.pop()
+    def displayStack(self):
+        print("Elements in Stack:")
+        for i in self.element:
+            print(i)
+
+s1 = Stack()
+s2 = Stack()
+
+s1.push()
+s1.push
+s1.pop()
+s1.displayStack()
---
+++ CW_TK_GUI (SIMPLE).py
@@ -0,0 +1,11 @@
+#import tkinter as tk
+
+# Create a window
+#root = tk.Tk()
+
+# Add a label
+#label = tk.Label(root, text="Hello, world!")
+#label.pack()
+
+# Keep the window open
+#root.mainloop()
